55245bccad612bfe48fac875f9ba0f141ccc990f
Mark Ortal <mark@kodecraft.dev>
2024-04-17 15:26:15 +0800

    Version: 1.0.0

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,37 @@
 use std::vec;
 
-use payoff::payoff::{calculate_payoff, OptionData, OptionDataSet};
+use payoff::payoff::{calculate_payoff, OptionData, OptionDataSet, OptionKind, Side};
 
 fn main(){
     let option1 = OptionData {
         strike: 64584.0,
         spot: 63153.43,
-        option_kind: "Call".to_string(),
-        side: "Buy".to_string(),
+        option_kind: OptionKind::CALL,
+        side: Side::BUY,
         ccy2: 96.2,
     };
 
     let option2 = OptionData {
         strike:63384.0,
         spot: 63153.43,
-        option_kind: "Call".to_string(),
-        side: "Sell".to_string(),
+        option_kind: OptionKind::CALL,
+        side: Side::SELL,
         ccy2: 439.16,
     };
 
     let option3 = OptionData {
         strike:61721.0,
         spot: 63153.43,
-        option_kind: "Put".to_string(),
-        side: "Buy".to_string(),
+        option_kind: OptionKind::PUT,
+        side: Side::BUY,
         ccy2: 63.69,
     };
 
     let option4 = OptionData {
         strike:62921.0,
         spot: 63153.43,
-        option_kind: "Put".to_string(),
-        side: "Sell".to_string(),
+        option_kind: OptionKind::PUT,
+        side: Side::SELL,
         ccy2: 347.77,
     };
 
@@ -42,7 +42,7 @@ fn main(){
     option_data_set.options.push(option3);
     option_data_set.options.push(option4);
 
-    let payoff = calculate_payoff(option_data_set, 63153.43);
+    let payoff = calculate_payoff(option_data_set, 63153.43, Option::None);
     print!("{:?}",vec!["test","test2"]);
     println!("{:?}", payoff);
 }
--- src/payoff.rs
+++ src/payoff.rs
@@ -1,20 +1,36 @@
 use serde::{Deserialize, Serialize};
 use ndarray::Array1;
 
-#[derive(Debug, Serialize, Deserialize, Clone, Default,PartialEq)]
+#[derive(Debug, Serialize, Deserialize, Clone, Default)]
+pub enum Side {
+    #[default]
+    BUY,
+    SELL,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone, Default)]
+pub enum OptionKind {
+    #[default]
+    CALL,
+    PUT,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone, Default)]
 pub struct OptionDataSet {
     pub options: Vec<OptionData>
 }
 
-#[derive(Debug, Serialize, Deserialize, Clone, Default, PartialEq)]
+#[derive(Debug, Serialize, Deserialize, Clone, Default)]
 pub struct OptionData {
     pub strike: f64,
     pub spot: f64,
-    pub option_kind: String,
-    pub side: String,
+    pub option_kind: OptionKind,
+    pub side: Side,
     pub ccy2: f64,
 }
 
+/// Payoff
+/// Struct for storing the vectors of x and y values
 #[derive(Debug, Serialize, Deserialize, Clone, Default,PartialEq)]
 pub struct Payoff{
     pub x: Vec<f64>,
@@ -25,57 +41,96 @@ fn format_currency(number: f64, scale: u8) -> String {
     format!("{:.*}", usize::from(scale), number)
 }
 
-// Calculate call option payoff
-fn call_option_formula(strike: f64, price: f64, premium: f64, direction: String) -> f64 {
-    let call_option = if direction.to_uppercase() == "BUY" {
-        (price - strike).max(0.0) - premium
-    } else {
-        premium - (price - strike).max(0.0)
-    };
-    call_option
-}
-
-//Calculate put option payoff
-fn put_option_formula(strike: f64, price: f64, premium: f64, direction: String) -> f64 {
-    let put_option = if direction.to_uppercase() == "BUY" {
-        (strike - price).max(0.0) - premium
-    } else {
-        premium - (strike - price).max(0.0)
-    };
-    put_option
-}
-
-fn calculate_total_payoff(option_data: &OptionData, price: f64) -> f64 {
-    match option_data.option_kind.as_str() {
-        "Call" => {
-            if option_data.side == "Buy" {
-                call_option_formula(option_data.strike, price, option_data.ccy2, "BUY".to_string())
-            } else {
-                call_option_formula(option_data.strike, price, option_data.ccy2, "SELL".to_string())
+fn calculate_axis_values(strike: f64, price: f64, premium: f64, side: Side, option_kind: OptionKind) -> f64 {
+    match option_kind {
+        OptionKind::CALL => {
+            match side {
+                Side::BUY => (price - strike).max(0.0) - premium,
+                Side::SELL => premium - (price - strike).max(0.0),
+            }
+        },
+        OptionKind::PUT => {
+            match side {
+                Side::BUY => (strike - price).max(0.0) - premium,
+                Side::SELL => premium - (strike - price).max(0.0),
             }
         }
-        "Put" => {
-            if option_data.side == "Buy" {
-                put_option_formula(option_data.strike, price, option_data.ccy2, "BUY".to_string())
-            } else {
-                put_option_formula(option_data.strike, price, option_data.ccy2, "SELL".to_string())
-            }
-        }
-        _ => 0.0,
     }
 }
 
-pub fn calculate_payoff(option_data_set: OptionDataSet, spots: f64) -> Payoff {
+fn calculate_total_payoff(option_data: &OptionData, price: f64) -> f64 {
+    match option_data.side {
+        Side::BUY => calculate_axis_values(option_data.strike, price, option_data.ccy2, Side::BUY, option_data.option_kind.clone()),
+        Side::SELL => calculate_axis_values(option_data.strike, price, option_data.ccy2, Side::SELL, option_data.option_kind.clone()),
+    }
+}
+/// Calculate the payoff for a given set of options
+/// Arguments:
+/// * `option_data_set` - A set of options
+/// * `spot` - The spot price
+/// * `dimension` - The number of points to calculate the payoff for
+/// 
+/// Returns:
+/// * `Payoff` - The payoff for the given set of options
+/// 
+/// # Example
+/// ```
+/// let option1 = OptionData {
+///     strike: 64584.0,
+///     spot: 63153.43,
+///     option_kind: OptionKind::CALL,
+///     side: Side::BUY,
+///     ccy2: 96.2,
+///     };
+///
+///     let option2 = OptionData {
+///     strike:63384.0,
+///     spot: 63153.43,
+///     option_kind: OptionKind::CALL,
+///     side: Side::SELL,
+///     ccy2: 439.16,
+///     };
+///     
+///     let option3 = OptionData {
+///     strike:61721.0,
+///     spot: 63153.43,
+///     option_kind: OptionKind::PUT,
+///     side: Side::BUY,
+///     ccy2: 63.69,
+///     };
+///
+///     let option4 = OptionData {
+///     strike:62921.0,
+///     spot: 63153.43,
+///     option_kind: OptionKind::PUT,
+///     side: Side::SELL,
+///     ccy2: 347.77,
+///     };
+///
+// Creating OptionDataSet and populating it with options
+///     let mut option_data_set = OptionDataSet::default();
+///     option_data_set.options.push(option1);
+///     option_data_set.options.push(option2);
+///     option_data_set.options.push(option3);
+///     option_data_set.options.push(option4);
+///
+///     let payoff = calculate_payoff(option_data_set, 63153.43, Option::None);
+/// ```
+pub fn calculate_payoff(option_data_set: OptionDataSet, spot: f64, dimension: Option<usize>) -> Payoff {
+    let dim = match dimension {
+        Some(d) => d,
+        None => 400,
+    };
     let mut payoff = Payoff::default();
     let std_dev = 0.1;
-    let upper_bound = spots * (1.0 + std_dev);
-    let lower_bound = spots * (1.0 - std_dev);
-    let x = Array1::linspace(lower_bound, upper_bound, 400).to_vec();
+    let upper_bound = spot * (1.0 + std_dev);
+    let lower_bound = spot * (1.0 - std_dev);
+    let x = Array1::linspace(lower_bound, upper_bound, dim).to_vec();
     let y = x.iter().map(|&price| {
         let payoff: f64 = option_data_set.options.iter().map(|option_data| {
-            format_currency(calculate_total_payoff(option_data, price), 2).parse::<f64>().unwrap()
+            calculate_total_payoff(option_data, price)
         }).sum();
-        payoff
+        format_currency(payoff, 2).parse::<f64>().unwrap()
     }).collect();
     payoff.x = x;
     payoff.y = y;
